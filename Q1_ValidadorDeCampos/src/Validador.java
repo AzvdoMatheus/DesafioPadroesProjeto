a7f1a883ae4ebd884d0a9f27253f5bf3f47d60e6
AzvdoMatheus <matheusjefremovas@gmail.com>
2024-08-28 18:28:00 -0300

    teste

--- Q1_ValidadorDeCampos/src/Validador.java
+++ Q1_ValidadorDeCampos/src/Validador.java
@@ -1,6 +1,9 @@
 public class Validador{
     public enum Tipo { EMAIL, INTEIRO, MATRICULA }
 
+
+
+    
     public boolean valida(Tipo tipo,String valor){
         switch(tipo){
             case INTEIRO:
